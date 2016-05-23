354a0ebdcea39d929af14418795ef0cbde1195e9
ffrancieli <francielifrv@gmail.com>
2016-05-22 23:49:40 -0300

    Bob answers to prolonged silence

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -6,19 +6,21 @@ class Bob {
       return "Whoa, chill out!"
     } else if(isQuestion(statement)){
       return "Sure."
+    } else if(statement.trim.isEmpty){
+      return "Fine. Be that way!"
     }
     "Whatever."
   }
 
-  def itIsOnlyUpperCase(statement:String): Boolean ={
+  def itIsOnlyUpperCase(statement:String): Boolean = {
     statement.toUpperCase == statement
   }
 
-  def containsLetter(statement: String): Boolean={
+  def containsLetter(statement: String): Boolean = {
     statement.matches(".*[A-Z].*")
   }
 
-  def isQuestion(statement:String): Boolean ={
+  def isQuestion(statement:String): Boolean = {
     statement.endsWith("?")
   }
 }
