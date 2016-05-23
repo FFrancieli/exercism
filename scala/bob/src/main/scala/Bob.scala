a99d12d14062d75aa4cca11bac596171c2b0a13c
ffrancieli <francielifrv@gmail.com>
2016-05-22 23:28:51 -0300

    Bob answers a screaming

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,5 +1,12 @@
 class Bob {
   def hey(statement:String) : String = {
+    if (containsOnlyUpperCaseLetters(statement)){
+      return "Whoa, chill out!"
+    }
     "Whatever."
+  }
+
+  def containsOnlyUpperCaseLetters(statement:String): Boolean ={
+    statement.toUpperCase == statement
   }
 }
