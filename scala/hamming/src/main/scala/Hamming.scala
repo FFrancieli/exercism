fda7863435655e169d24d4f4779c32291737ac07
ffrancieli <francielifrv@gmail.com>
2016-05-23 22:08:34 -0300

    Remove unnecessary comment

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,11 +1,7 @@
-/**
-  * Created by francieli on 23/05/16.
-  */
 object Hamming {
 
   def compute(dnaStrand: String, equivalentDnaStrand:String): Int = {
     if(!sameLenght(dnaStrand, equivalentDnaStrand)) throw new IllegalArgumentException
-
     dnaStrand.zip(equivalentDnaStrand).count {
       case (charDna, charDnaEquivalent) => charDna != charDnaEquivalent
     }
