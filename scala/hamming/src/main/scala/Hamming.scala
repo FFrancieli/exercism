aa2b63d6fd84268278f72c21db716a4f49699dd4
ffrancieli <francielifrv@gmail.com>
2016-05-23 22:44:51 -0300

    Refactor function sameLength to verify if strings have different lengths

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,13 +1,14 @@
 object Hamming {
 
   def compute(dnaStrand: String, equivalentDnaStrand:String): Int = {
-    if(!sameLenght(dnaStrand, equivalentDnaStrand)) throw new IllegalArgumentException
+    if(differentLenght(dnaStrand, equivalentDnaStrand)) throw new IllegalArgumentException
+
     dnaStrand.zip(equivalentDnaStrand).count {
       case (charDna, charDnaEquivalent) => charDna != charDnaEquivalent
     }
   }
 
-  def sameLenght(string: String, anotherString: String):Boolean = {
-    string.length.equals(anotherString.length)
+  def differentLenght(string: String, anotherString: String):Boolean = {
+    string.length != anotherString.length
   }
 }
