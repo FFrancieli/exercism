95a25b1a8a936abcdc374c91ed160b83127489ca
Francieli Viane <francielifrv@gmail.com>
2016-07-09 22:33:58 -0300

    Replacing function replaceFirst with tail

--- scala/phone-number/src/main/scala/PhoneNumber.scala
+++ scala/phone-number/src/main/scala/PhoneNumber.scala
@@ -5,11 +5,9 @@ class PhoneNumber(phoneNumber: String) {
   def getPhoneNumber(): String = {
     val number:String = phoneNumber.filter(_.isDigit)
 
-    if (isValidNumber(number)) number.replaceFirst("1", "") else number
+    if (isValidNumber(number)) number.tail else number
   }
 
-  def isValidNumber(number:String): Boolean = {
-    number.length == 11 && number.take(1).equals("1")
-  }
+  def isValidNumber(number:String): Boolean = number.length == 11 && number.take(1).equals("1")
 
 }
