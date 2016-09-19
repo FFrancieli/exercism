45d435aa369dfb1d5443e63f9577df3b39593ca5
ffrancieli <francielifrv@gmail.com>
2016-09-18 22:55:59 -0300

    Turning function differentLenght into a curried function

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,14 +1,13 @@
 object Hamming {
 
   def compute(dnaStrand: String, equivalentDnaStrand:String): Int = {
-    if(differentLenght(dnaStrand, equivalentDnaStrand)) throw new IllegalArgumentException
+    if(differentLenght(dnaStrand) (equivalentDnaStrand)) throw new IllegalArgumentException
 
     (dnaStrand zip equivalentDnaStrand).count{
       case (charDna, charDnaEquivalent) => charDna != charDnaEquivalent
     }
   }
 
-  def differentLenght(string: String, anotherString: String):Boolean = {
-    string.length != anotherString.length
-  }
+  private def differentLenght(string: String) =
+    (anotherString:String) => string.length != anotherString.length
 }
