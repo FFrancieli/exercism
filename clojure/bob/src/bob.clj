30619d1aef37804783a41e8885f0f0120ae49d80
ffrancieli <francielifrv@gmail.com>
2016-11-25 19:35:00 -0200

    Bob answers to silence

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -5,7 +5,12 @@
   (re-find #"[A-Z]+" statment))
 )
 
+(defn- is-silence?  [statment]
+  (= (.trim statment) "")
+)
+
 (defn response-for [statment]
   (cond
     (is-shouting? statment) "Whoa, chill out!"
+    (is-silence? statment) "Fine. Be that way!"
     :else "Whatever."))
