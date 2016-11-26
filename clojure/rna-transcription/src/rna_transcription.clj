eb70255065a56dd11dec5d91f1e40f08bca0f924
ffrancieli <francielifrv@gmail.com>
2016-11-26 21:17:21 -0200

    converts a sequence of DNA nucleotides to RNA

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,12 +1,11 @@
 (ns rna-transcription)
 
 (def dna-to-rna {
-  "G" "C"
-  "C" "G"
-  "T" "A"
-  "A" "U"
+  \G \C
+  \C \G
+  \T \A
+  \A \U
 })
 
 (defn to-rna [dna-strand]
-  (get dna-to-rna dna-strand)
-)
+  (apply str (map #(get dna-to-rna %) dna-strand)))
