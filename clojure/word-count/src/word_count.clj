f07443889b3558a71f7328a11079499621bda623
ffrancieli <francielifrv@gmail.com>
2016-11-26 22:37:11 -0200

    counts words in a phrase
    but still ignores punctuation and other symbols

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,10 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :as string]))
 
-(defn word-count [] ;; <- arglist goes here
-  ;; your code goes here
+(defn- list-of-words [phrase]
+  (string/split phrase #" ")
+)
+
+(defn word-count [phrase]
+  (frequencies (list-of-words phrase))
 )
