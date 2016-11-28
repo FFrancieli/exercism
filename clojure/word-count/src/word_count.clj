15d1abe400fff5226b3b4cd6ea586d3810eec961
ffrancieli <francielifrv@gmail.com>
2016-11-28 20:21:36 -0200

    Refactoring in order to improve readability

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,10 +1,11 @@
 (ns word-count
   (:require [clojure.string :as string]))
 
-(defn- list-of-words [phrase]
-  (filter #(not (empty? %)) (string/split phrase #"\W"))
-)
+(defn- remove-blank-items-in[words-list]
+  (filter #(not (empty? %)) words-list))
+
+(defn- generate-list-of-words [phrase]
+  (remove-blank-items-in (string/split phrase #"\W")))
 
 (defn word-count [phrase]
-  (frequencies (list-of-words (string/lower-case phrase)))
-)
+  (frequencies (generate-list-of-words (string/lower-case phrase))))
