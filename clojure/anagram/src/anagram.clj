14c7c7d908d093c455acf625b369aab976788d9e
Francieli Viane <francielifrv@gmail.com>
2016-12-17 12:09:50 -0200

    identifies anagrams

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,11 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as string]))
 
-(defn anagrams-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- is-anagram? [word possible-anagram]
+  (cond
+    (= word possible-anagram) false
+    (= (sort word) (sort possible-anagram)) true))
+
+(defn anagrams-for [word word-list]
+  (filter (fn [possible-anagram]
+            (is-anagram? word possible-anagram)) word-list))
