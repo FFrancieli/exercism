d4f4c8362849108b43f80aba4b5b22f380cc47b5
Francieli Viane <francielifrv@gmail.com>
2016-12-18 14:14:22 -0200

    [clojure] improving readability

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,12 +1,18 @@
 (ns anagram
   (:require [clojure.string :as string]))
 
+(defn- contains-exactly-same-letters? [word possible-anagram]
+  (= (sort word) (sort possible-anagram)))
+
+(defn- same-word? [word word-to-compare]
+  (= word word-to-compare))
+
 (defn- anagram? [word possible-anagram]
-  (cond
-    (= word possible-anagram) false
-    (= (sort word) (sort possible-anagram)) true))
+  (and (not (same-word? word possible-anagram))
+       (contains-exactly-same-letters? word possible-anagram)))
 
 (defn anagrams-for [word word-list]
   (filter (fn [possible-anagram]
-            (anagram? (string/lower-case word) (string/lower-case possible-anagram)))
+            (anagram? (string/lower-case word)
+                      (string/lower-case possible-anagram)))
           word-list))
