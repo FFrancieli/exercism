8dcdfcc6d7f340b8d6e199642d8eb02a17daac57
Francieli Viane <francielifrv@gmail.com>
2016-12-18 13:50:15 -0200

    [clojure] rename method is-anagram? to anagram?

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,12 +1,12 @@
 (ns anagram
   (:require [clojure.string :as string]))
 
-(defn- is-anagram? [word possible-anagram]
+(defn- anagram? [word possible-anagram]
   (cond
     (= word possible-anagram) false
     (= (sort word) (sort possible-anagram)) true))
 
 (defn anagrams-for [word word-list]
   (filter (fn [possible-anagram]
-            (is-anagram? (string/lower-case word) (string/lower-case possible-anagram)))
+            (anagram? (string/lower-case word) (string/lower-case possible-anagram)))
           word-list))
