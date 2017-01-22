2bf8a919a96db5b60af6a3e36ab93784f2e331c4
ffrancieli <francielifrv@gmail.com>
2017-01-22 21:50:32 -0200

    [Clojure] - nucleotide-count: counts nucleotides ocurrences in a given DNA strand

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,7 +1,11 @@
 (ns nucleotide-count)
 
+(def nucleotides (list \A \C \G \T))
+
 (defn count [necleotide dna-sequence] 
 	(get (frequencies dna-sequence) necleotide 0))
 
 
-(defn nucleotide-counts [dna-sequence])
+(defn nucleotide-counts [dna-sequence]
+	(merge (zipmap nucleotides (repeat 0)) (frequencies dna-sequence) ))
+	
