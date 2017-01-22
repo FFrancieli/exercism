3001223946028f96dcbc9bdd439c603ced5ab676
ffrancieli <francielifrv@gmail.com>
2017-01-22 21:56:24 -0200

    [Clojure] - nucleotide-count: refactoring to reuse code

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,11 +1,9 @@
 (ns nucleotide-count)
 
-(def nucleotides (list \A \C \G \T))
-
-(defn count [necleotide dna-sequence] 
-	(get (frequencies dna-sequence) necleotide 0))
-
+(def ^:private nucleotides (list \A \C \G \T))
 
 (defn nucleotide-counts [dna-sequence]
-	(merge (zipmap nucleotides (repeat 0)) (frequencies dna-sequence) ))
-	
+	(merge (zipmap nucleotides (repeat 0)) (frequencies dna-sequence)))
+
+(defn count [necleotide dna-sequence] 
+	(get (nucleotide-counts dna-sequence) necleotide))
